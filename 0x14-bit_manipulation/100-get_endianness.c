eca2999d061c0205e41f0399ff2084e2f64884cc
henok1992 <henokgithub@gmail.com>
2022-07-22 02:03:44 -0700

    seghb

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * get_endianness - Checks the systems Byte order
- * Return: Returns 0 if big endian 1 if little endian
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
-	int x;
-	int little_or_big;
+	unsigned int x;
+	char *c;
 
 	x = 1;
-	little_or_big = (int) (((char *)&x)[0]);
-	return (little_or_big);
+	c = (char *) &x;
+
+	return ((int)*c);
 }
